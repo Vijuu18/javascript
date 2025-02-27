06149311807da3188828272734e37e6cdb124d0b
Vijuu18 <vp6903548@gmail.com>
2025-02-27 05:30:22 +0000

    container config done

---
+++ 01.basics/02datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // this means its treat all code as newer version of js
+//alert ( 3 + 3) // we using nodejs not browser
+console.log(3+3);
+
+let name="vijay"
+let age = 20
+let isLoggedIn = false
+
+// number = 2*53
+// bigint
+// string=" "
+// boolean = true/false
+// null= standalone value
+// undefined
+// symbol
+// 
+
+//object
+console.log(typeof"vijay");
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+
