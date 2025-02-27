bcc23a9a2d9647fb462fedbefd8351fa59c90caf
Vijuu18 <vp6903548@gmail.com>
2025-02-27 04:52:15 +0000

    container config done

--- 01.basics/01variable.js
+++ 01.basics/01variable.js
@@ -2,9 +2,17 @@ const accountId = 1234
 let accountEmail = "vijay@1001.com"
 var accountPassword = "1121"
 accountCity = "indore"
+let accountState
 
+accountEmail="vijaypatel18@.com"
+accountPassword="121212"
+accountCity="ujjain"
 //accountId = 23
+
+// prefer not to use var use let instead
+//  because it has issues in functional scope  and block scope
 console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
 
 
 
