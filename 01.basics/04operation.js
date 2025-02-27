1a16aa429cf1dbb371fe995ef8087fde08b3f72e
Vijuu18 <vp6903548@gmail.com>
2025-02-27 07:02:23 +0000

    go

--- 01.basics/04operation.js
+++ 01.basics/04operation.js
@@ -2,24 +2,24 @@ let value=9
 let negvalue= -value
 console.log(negvalue);
 
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**4);
-// console.log(2/3);
-// console.log(3%2);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**4);
+console.log(2/3);
+console.log(3%2);
 
 let str1="hello"
 let str2 =" Vishab"
 let str3= str1+ str2
-// console.log(str3);
-// console.log(2+"2");
-// console.log("3"+4);
-// console.log(1+2+"3");
-// console.log("2"+2+3);
+console.log(str3);
+console.log(2+"2");
+console.log("3"+4);
+console.log(1+2+"3");
+console.log("2"+2+3);
 
-// console.log(+true);
-// console.log(+"");
+console.log(+true);
+console.log(+"");
 
 let num1,num2,num3
 num1=num2=num3= 3+3
---
+++ 01.basics/05comparison.js
@@ -0,0 +1,20 @@
+console.log(2>1);
+console.log(2<1);
+console.log(21>=1);
+console.log(2!=1);
+console.log(2==1);
+
+console.log("2">1);
+console.log(2<"1");
+
+console.log(null >0);
+console.log(null ==0);
+console.log(null >=0);
+// comparison operator treated null as 0 
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+console.log("2"===2); 
+// === its check datatypes also
