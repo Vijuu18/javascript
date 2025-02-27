59ae4c7c59cb5a93059f7631c771e85758b607f6
Vijuu18 <vp6903548@gmail.com>
2025-02-27 06:04:24 +0000

    conversion

---
+++ 01.basics/03conversion.js
@@ -0,0 +1,32 @@
+let score = 33
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+ 
+// 33=> 33
+// 33abc = NaN
+// true=1; false=0
+// null =0
+// undefined = NaN (not a number)
+
+let isLoggedIn= 1
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1= true; 0=false
+// ""=false
+// "vijay"= true 
+ 
+let someNumber= "45"
+let boolsomeNumber = String(someNumber)
+console.log(boolsomeNumber);
+console.log(typeof boolsomeNumber);
+
+
+
+
+
+
+
