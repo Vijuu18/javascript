7264856b8a4c3e639000ba40ce830bc77bbde9db
Vijuu18 <vp6903548@gmail.com>
2025-03-01 04:50:02 +0000

    ok

---
+++ 01.basics/06datatypesbasics.js
@@ -0,0 +1,22 @@
+const id =Symbol("12")
+const newid=Symbol("12")
+console.log(id==newid);
+
+const val=100.3 // number datatype me saare hi aagye float bhi
+const bignum=728273288223828n  // n lagane ke baad ye bigint me convert ho gaya
+
+//array
+const hero=["shaktiman","ironman","captain"]
+console.log(hero);
+
+//object
+let myobj={
+    name:"vijay",
+    age:20,
+}
+// function
+const myfun= function(){
+    console.log("hello world");
+}
+
+console.log(typeof bignum);
