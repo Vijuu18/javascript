0344f8af320fae4ca67d6a22fe7cb0f1f0a40b38
Vijuu18 <vp6903548@gmail.com>
2025-03-01 06:35:54 +0000

    strings

---
+++ 01.basics/07strings.js
@@ -0,0 +1,38 @@
+const name="vijay-kumar-thalapaty"
+const count=30
+
+console.log(name+ count+ " value");
+console.log(`my name is ${name} my count is ${count}`);
+
+const gamename= new String("vijjayy");
+console.log(gamename);
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(4));
+console.log(gamename.indexOf('j'));
+
+const a = gamename.substring(0 ,4)
+console.log(a);
+const b = gamename.slice(-6,4)
+console.log(b);
+
+const mama="    kuldeep    "
+console.log(mama);
+console.log(mama.trim());
+
+const url="https://vijayq79vijaypatidar.com"
+console.log(url.replace('q79','-'));
+
+console.log(url.includes("vijay"));
+
+console.log(name.split('-',2));  // aage wale me ye he ki kis chiz se separate hota h ,baad me limit h 
+
+
+
+
+
+
+
+
