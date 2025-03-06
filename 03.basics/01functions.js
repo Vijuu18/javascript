7078b5eb2af40eb20cdddaeb33d38d1de7edfb6a
Vijuu18 <vp6903548@gmail.com>
2025-03-06 04:06:54 +0000

    functions

--- 03.basics/01functions.js
+++ 03.basics/01functions.js
@@ -17,3 +17,33 @@ function loginUserMsg(username="harsh"){
     return `${username} just logged in`
 }
 console.log(loginUserMsg("vijay"));
+
+function calculateCartPrice(val1,...num1){  // three dot is a spread or rest operator,sabko ek me kar dega ya alag alag 
+    return num1
+}
+console.log(calculateCartPrice(100,200,300));
+
+const user={
+    username:"vijay",
+    price:1999
+}
+
+function handleObject(anyobject){
+    console.log(`my name is ${anyobject.username} my salary is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({    // object bana kar bhi bhej sakte hai
+    username:"vinay",
+    price:3999
+})
+ 
+const myNewArray=[100,200,300,340]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([230,340,340,234]));// we can directly pass a array in the function
+
