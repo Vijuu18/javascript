faad7015ae29f916acd8d1e50c53d107b7dc27e6
Vijuu18 <vp6903548@gmail.com>
2025-03-07 04:40:40 +0000

    switcj

---
+++ 04.control_flow/02switch.js
@@ -0,0 +1,21 @@
+// const month=3
+const month="march"
+// ctrl + alt + down key  for duplicte
+switch (month) {
+    case "jan":               // number ki gajah string bhi likh sakte h,compare karne ke liye
+        console.log("jan");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default");
+        break;
+}
